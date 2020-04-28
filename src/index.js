89a3d52d785c23d357834ea5a3c473af47968582
Paul Rosero <paul_rosero@yahoo.com>
2020-04-27 23:14:33 -0400

    add import store from ./store.js.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from "redux-thunk";
 import { Provider } from "react-redux"
-
-const store = createStore(reducer, )
+import store from './store.js'
 
 ReactDOM.render(
   <Provider store={store}>
