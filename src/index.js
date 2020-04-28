86f9408c07d3ee7eec63ea04fa3e3509faaac595
Paul Rosero <paul_rosero@yahoo.com>
2020-04-27 22:37:23 -0400

    add store and provider. Getting things ready for reducers and actions.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from "redux-thunk";
+import { Provider } from "react-redux"
+
+const store = createStore(reducer, )
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
