ee64237605bc9ca43e55b04a6cd22d941557dec9
Paul Rosero <paul_rosero@yahoo.com>
2020-04-29 17:49:39 -0400

    delete argument from getCurrentUser and added get_current_user to route.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -7,17 +7,43 @@ export const setCurrentUser = user => {
 }
 
 
-
-
 //asynchronous action creators
-export const login = loginData => {
+export const currentUserLogin = loginData => {
+    console.log(loginData)
     return dispatch => {
-        return fetch("http://localhost:3001", {
+        return fetch("http://localhost:3001/api/v1/login", {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(loginData)
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+    }
+}
+
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/get_current_user", {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(res => res.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
         })
     }
 }
