2864d4415b6d7fb806693d550a5324cea0bb640c
Paul Rosero <paul_rosero@yahoo.com>
2020-04-30 14:30:05 -0400

    import resetLoginForm and use it in line 35.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,3 +1,5 @@
+import { resetLoginForm } from './loginForm.js';
+
 //synchronous action creators
 export const setCurrentUser = user => {
     return {
@@ -30,6 +32,7 @@ export const currentUserLogin = loginData => {
         }
         else {
             dispatch(setCurrentUser(user.data))
+            dispatch(resetLoginForm())
         }
     }
 }
