ceb2dc5198ddbf7a1ffa6c04ff7d443054c88e54
Paul Rosero <paul_rosero@yahoo.com>
2020-04-30 12:12:03 -0400

    take code not needed and added it to components/NavBar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login'
-import Logout from './components/Logout'
 import { connect } from 'react-redux';
 import { getCurrentUser} from './actions/currentUser'
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
 
@@ -11,15 +10,12 @@ class App extends React.Component {
     this.props.getCurrentUser()
   }
 
-  render(){    
+  render() {    
     return (
-      this.props.currentUser ? <Logout /> : <Login />
-    );
+      <NavBar />
+    )
   }
 }
 
-const mapStateToProps = ({currentUser}) => {
-  return {currentUser}
-}
 
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default connect(null, { getCurrentUser })(App);
