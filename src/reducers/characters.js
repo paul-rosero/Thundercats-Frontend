f2820b44d3a7d2d03b9952349b9978875474577f
Paul Rosero <paul_rosero@yahoo.com>
2020-05-04 15:23:40 -0400

    add reducercase and return the characters.

--- src/reducers/characters.js
+++ src/reducers/characters.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
     switch (action.type) {
+        case "SET_MY_CHARACTERS" :
+            return action.characters
         default :
             return state
     }
