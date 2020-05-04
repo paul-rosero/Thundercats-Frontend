5bce6c9839e159c7daccee03e2ca740b9537e00b
Paul Rosero <paul_rosero@yahoo.com>
2020-05-04 16:54:38 -0400

    change name from Thundercats.js to CharactersContainer and everywhere else inside the files.

---
+++ src/containers/CharactersContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Characters from "../components/Characters.js";
+
+const CharactersContainer = () => {
+    return(
+        <div>
+          <Characters />
+        </div>
+    )
+}
+export default CharactersContainer;
