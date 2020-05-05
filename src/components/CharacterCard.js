2247ef52517c2dfc854d32a6d81fcbfcd92ae7c8
Paul Rosero <paul_rosero@yahoo.com>
2020-05-04 20:56:59 -0400

    change name from CharsCard to CharactersCard to sound better.

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const CharacterCard = ({characters}) => {
+    
+    return (
+          <div className="CharacterCard">
+            <p>{characters.attributes.name}</p>
+          </div>
+    )
+      
+    
+}
+
+export default CharacterCard;
