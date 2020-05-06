7f7e04af9710f8745b8e469727a80d70f992780b
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 16:56:21 -0400

    comment out some code im not using for the moment, but not sure if ill need it.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Login from './Login'
-import Logout from './Logout'
+// import Login from './Login'
+// import Logout from './Logout'
 
 const NavBar = ({ currentUser }) => {
     
     return (
        <div className="navbar" >
-            <div className="greeting">
+            {/* <div className="greeting">
                 { currentUser ? <h3>Welcome {currentUser.attributes.name}!</h3> : <h3>Welcome Thundercat, Please login</h3>}
-            </div>
-            <div className="nav-button">
+            </div> */}
+            {/* <div className="nav-button">
                 { currentUser ? <Logout /> : <Login /> }
-            </div>        
+            </div>         */}
        </div>
     )
 }
