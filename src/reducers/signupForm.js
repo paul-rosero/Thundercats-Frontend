e8cb0aa5508fc3c8f47f1faae87e5c59114588d3
Paul Rosero <paul_rosero@yahoo.com>
2020-05-05 23:45:52 -0400

    add case for update and reset signup form and set initialState var.

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -6,8 +6,10 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        // case "" :
-        //     return action.
+        case "UPDATE_SIGNUP_FORM" :
+            return action.formData
+        case "RESET_SIGNUP_FORM" :
+            return initialState
         default :
             return state
     }
