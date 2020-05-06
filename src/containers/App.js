146379657b99b21b42b2861b8b069ac3a3f574a4
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 16:57:54 -0400

    import withBrowser to be able to add props to react.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,34 +1,45 @@
-//stateless component4
+
 import React from 'react';
 import '../App.css';
 import { connect } from 'react-redux';
 import { getCurrentUser} from '../actions/currentUser';
 import NavBar from '../components/NavBar';
 import Login from '../components/Login';
+import Logout from '../components/Logout';
 import Signup from '../components/Signup';
-import CharactersContainer from './CharactersContainer';
-import { Route } from 'react-router-dom'
+import ProfilePage from './ProfilePage.js';
+
+import Home from './Home';
+import { Route, Switch, withRouter } from 'react-router-dom'
+
 class App extends React.Component {
 
   componentDidMount() {
     this.props.getCurrentUser()
   }
 
-  render() {    
+  render() {
+    const { loggedIn } = this.props   
     return (
-      <div className="App">
-        <NavBar />
-        <CharactersContainer />
+      <div className="App">   
+        {loggedIn ? <Logout/> : null}
         <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={Signup} />
+        <Route exact path='/' render={(props) => loggedIn ? <ProfilePage {...props}/> : <Home {...props} />} />
         {/* <Route exact path='' component={} /> */}
         {/* <Route exact path='' component={} /> */}
-        {/* <Route exact path='' component={} /> */}
+  
+        
       </div>
       
     )
   }
 }
 
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+}
 
-export default connect(null, { getCurrentUser })(App);
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
