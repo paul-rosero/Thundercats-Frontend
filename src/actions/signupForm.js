94229445e0ec9c72de3e464e8d93dd668d995f8d
Paul Rosero <paul_rosero@yahoo.com>
2020-05-05 23:42:44 -0400

    add code to create actions to update and reset signup form.

---
+++ src/actions/signupForm.js
@@ -0,0 +1,13 @@
+//stateless component3
+export const updateSignupForm = formData => {
+    return {
+        type: "UPDATE_SIGNUP_FORM",
+        formData
+    }
+}
+
+export const resetSignupForm = () => {
+    return {
+        type: "RESET_SIGNUP_FORM"
+    }
+}
