cf1145e5b954fdbcaed824f41f97d4da4274c25a
Paul Rosero <paul_rosero@yahoo.com>
2020-05-05 22:43:21 -0400

    add code to have an initial state and then add cases later.

---
+++ src/reducers/signupForm.js
@@ -0,0 +1,14 @@
+const initialState = {
+    email: "",
+    username: "",
+    password: ""
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        // case "" :
+        //     return action.
+        default :
+            return state
+    }
+}
