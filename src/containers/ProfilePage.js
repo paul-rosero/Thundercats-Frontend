5a93f18921574eadff94c7ac7100faf0c98d3158
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 14:46:39 -0400

    add profilepage container for a user page.

---
+++ src/containers/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CharactersContainer from './CharactersContainer';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <CharactersContainer/>
+        </div>
+    )
+}
+
+export default ProfilePage
