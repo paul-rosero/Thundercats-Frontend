e5b19e536f36ef5e02819d0998327f323de79365
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 16:59:27 -0400

    add link to signup and login and comment out imports since im not using it for now.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,14 @@
 //stateless component 6
-import React from 'react'
+import React from 'react';
+// import Login from '../components/Login';
+// import Logout from '../components/Logout'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
-            
+            <h2>Welcome to the Thundercats Page!</h2>
+            <h4>You can <Link to='/signup'>Signup</Link> or <Link to='/login'>Login</Link></h4>
         </div>
     )
 }
