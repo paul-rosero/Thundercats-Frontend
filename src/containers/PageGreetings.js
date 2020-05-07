a7a479f38e514fafb69d2573f949b86df1a24bf7
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 23:41:39 -0400

    add greetings for login and signup functions to be used in the corresponding components.

---
+++ src/containers/PageGreetings.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const signupGreetings = () => {
+    return (
+        <div className="signup-greeting">
+            <h3>All are Welcomed in Third Earth.</h3>
+            <h5>Please Fill in all the information to create an account.</h5>
+        </div>
+    )
+}
+
+export const loginGreetings = () => {
+    return (
+        <div className="login-greeting">
+            <h3>Welcome back to Third Earth!</h3>
+            <h5>Please Sign in to enter.</h5>
+        </div>
+    )
+}
