7061e6f5d8bd5e94a7eb702664211fd4f2740859
Paul Rosero <paul_rosero@yahoo.com>
2020-05-07 16:50:20 -0400

    add navLinks and code that i will be using later.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+// import { NavLink } from 'react-router-dom'
+import Logout from './Logout'
 // import Login from './Login'
-// import Logout from './Logout'
 
-const NavBar = ({ currentUser }) => {
-    
+
+const NavBar = ({  loggedIn }) => {
     return (
        <div className="navbar" >
-            {/* <div className="greeting">
-                { currentUser ? <h3>Welcome {currentUser.attributes.name}!</h3> : <h3>Welcome Thundercat, Please login</h3>}
-            </div> */}
-            {/* <div className="nav-button">
-                { currentUser ? <Logout /> : <Login /> }
-            </div>         */}
+           {/* <NavLink exact to="/fav-characters">Fav Characters</NavLink> */}
+           {/* <NavLink exact to="/" ></NavLink> */}
+           {loggedIn ? <Logout/> : null}
        </div>
     )
 }
 
 const mapStateToProps = ({currentUser}) => {
-    return {currentUser}
+    return {
+        currentUser,
+        loggedIn: !!currentUser
+    }
   }  
 
 export default connect(mapStateToProps)(NavBar);
