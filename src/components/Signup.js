6b0ce8ca8cc7016fc7f68449abd3082d0ad8789e
Paul Rosero <paul_rosero@yahoo.com>
2020-05-06 23:40:56 -0400

    add signup greetings so a user is greetings with when signing up.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateSignupForm } from '../actions/signupForm.js'
 import { signup } from '../actions/currentUser.js'
+import { signupGreetings } from '../containers/PageGreetings.js';
+
 
 const Signup = ({signupForm, updateSignupForm, signup, history }) => {
   
@@ -19,22 +21,25 @@ const Signup = ({signupForm, updateSignupForm, signup, history }) => {
     }
    
     return (
-       
-        <form onSubmit={handleOnSubmit}>
-            <label>Name: </label>
-            <input name="name" type="text" onChange={handleInputChange} /><br/>
+        <div>
+            { signupGreetings() }
+            <form onSubmit={handleOnSubmit}>
+                <label>Name: </label>
+                <input name="name" type="text" onChange={handleInputChange} /><br/>
 
-            <label>Email: </label>
-            <input name="email" type="text" onChange={handleInputChange} /><br/>
+                <label>Email: </label>
+                <input name="email" type="text" onChange={handleInputChange} /><br/>
 
-            <label>Username: </label>
-            <input name="username" type="text" onChange={handleInputChange} /><br/>
+                <label>Username: </label>
+                <input name="username" type="text" onChange={handleInputChange} /><br/>
 
-            <label>Password: </label>
-            <input name="password" type="text" onChange={handleInputChange} /><br />
+                <label>Password: </label>
+                <input name="password" type="text" onChange={handleInputChange} /><br />
 
-            <input type="submit" value="Sign Up" />
-        </form>
+                <input type="submit" value="Sign Up" />
+            </form>
+        </div>
+        
     )
 }
 
