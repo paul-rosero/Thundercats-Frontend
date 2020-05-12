cc9f5aba3ecb2f8e1547590d1d9485c5f6d4a3e4
Paul Rosero <paul_rosero@yahoo.com>
2020-05-12 15:43:23 -0400

    add attributes to display.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-
-const CharacterCard = (props) => {
+const CharacterCard = ({location}) => {
  
-  console.log('props', props)
+  console.log('location', location.state.characters)
     return (
       <div className="CharacterCard">
-        <p>hi</p>
+        <p>Name: {location.state.characters.attributes.name}</p>
+        <p>Favorite: </p>
+        <p>Gender: {location.state.characters.attributes.gender}</p>
+        <p>Species: {location.state.characters.attributes.species}</p>
+        <p>Weapon(s): {location.state.characters.attributes.weapons}</p>        
+        <p>Overview: {location.state.characters.attributes.overview}</p>
       </div>
     )    
 }
