25c7a15d83b8f4632269a3f2e627f20014c100d1
Paul Rosero <paul_rosero@yahoo.com>
2020-05-13 16:46:36 -0400

    add className to line 5.

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UserInfo = ({currentUser}) => {
     return (
-        <div className="" >
+        <div className="current-user-information" >
             <p>Username: {currentUser.attributes.username}</p>
             <p>Email: {currentUser.attributes.email}</p>
             <p>Name: {currentUser.attributes.name}</p>
