031b43cf95ff36232ed40bbcbf6f54f1310db7b2
Paul Rosero <paul_rosero@yahoo.com>
2020-05-13 16:48:50 -0400

    delete code that was moved to other controllers and no longer needed here, clean up the container.

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CharHeadshots from './CharHeadshots';
+import UserInfo from '../components/UserInfo';
+import { profileGreetings } from './PageGreetings';
   
 const ProfilePage = ({currentUser}) => {
     return (
         <div className="profile-page">
-            <div className="greeting">
-                { currentUser ? <h3>Welcome {currentUser.attributes.name}!</h3> : <h3>Welcome Thundercat, Please login</h3>}
-            </div>
-            <div className="current-user-information">
-                <h5>Username: {currentUser.attributes.username}</h5>
-            </div>
-            <div className="characters-picture-container">
-            <table className="characters-picture-table">
-                <colgroup>
-                    <col span="2"/>
-                </colgroup>
-                <tbody >
-                    <CharHeadshots currentUser={currentUser}/>
-                </tbody>
-            </table>
-            </div>
-            
-            
+            { profileGreetings(currentUser) } 
+             <UserInfo currentUser={currentUser} />
+            <CharHeadshots currentUser={currentUser}/>
         </div>
             
     )
