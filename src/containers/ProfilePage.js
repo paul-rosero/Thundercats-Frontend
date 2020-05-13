4594de4802855d4197cdb378f1c9f839b179c82e
Paul Rosero <paul_rosero@yahoo.com>
2020-05-12 23:27:47 -0400

    delete console.log of current user.

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -8,11 +8,20 @@ const ProfilePage = ({currentUser}) => {
             <div className="greeting">
                 { currentUser ? <h3>Welcome {currentUser.attributes.name}!</h3> : <h3>Welcome Thundercat, Please login</h3>}
             </div>
-            <table>
-                <tbody>
+            <div className="current-user-information">
+                <h5>Username: {currentUser.attributes.username}</h5>
+            </div>
+            <div className="characters-picture-container">
+            <table className="characters-picture-table">
+                <colgroup>
+                    <col span="2"/>
+                </colgroup>
+                <tbody >
                     <CharHeadshots currentUser={currentUser}/>
                 </tbody>
             </table>
+            </div>
+            
             
         </div>
             
