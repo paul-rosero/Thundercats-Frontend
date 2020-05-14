3fa09f116f10b0734cc2248f2533aabefbd9cb4b
Paul Rosero <paul_rosero@yahoo.com>
2020-05-14 12:54:59 -0400

    refactor some code to make it more dry.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,14 +4,14 @@ import { updateSignupForm } from '../actions/signupForm.js'
 import { signup } from '../actions/currentUser.js'
 import { signupGreetings } from '../containers/PageGreetings.js';
 
-
-
 const Signup = ({signupForm, updateSignupForm, signup, history }) => {
-  
+    const { name, username, email, password } = signupForm
+
     const handleInputChange = event => {
+        const { name, value } = event.target
         const updatedFormInfo = {
             ...signupForm,
-            [event.target.name]: event.target.value
+            [name]: value
         }
       updateSignupForm(updatedFormInfo)
     }
@@ -26,21 +26,20 @@ const Signup = ({signupForm, updateSignupForm, signup, history }) => {
             { signupGreetings() }
             <form name="signup-form" onSubmit={handleOnSubmit}>
                 <label>Name: </label>
-                <input name="name" type="text" onChange={handleInputChange} /><br/>
+                <input name="name" type="text" value={name} onChange={handleInputChange} /><br/>
 
                 <label>Email: </label>
-                <input name="email" type="text" onChange={handleInputChange} /><br/>
+                <input name="email" type="text" value={email} onChange={handleInputChange} /><br/>
 
                 <label>Username: </label>
-                <input name="username" type="text" onChange={handleInputChange} /><br/>
+                <input name="username" type="text" value={username} onChange={handleInputChange} /><br/>
 
                 <label>Password: </label>
-                <input name="password" type="text" onChange={handleInputChange} /><br />
+                <input name="password" type="text" value={password} onChange={handleInputChange} /><br />
 
                 <input type="submit" value="Sign Up" />
             </form>
         </div>
-        
     )
 }
 
