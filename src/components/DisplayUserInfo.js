7a286cc9212bc5a3fe12f83802ab4fd2260757d2
Paul Rosero <paul_rosero@yahoo.com>
2020-05-15 17:31:30 -0400

    took out link to edit and moved it somewhere else.

--- src/components/DisplayUserInfo.js
+++ src/components/DisplayUserInfo.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-const DisplayUserInfo = ({currentUser}) => {
+const DisplayUserInfo = () => {
     return (
         <div className="current-user-information" >
             <p>Username: {currentUser.attributes.username}</p>
             <p>Email: {currentUser.attributes.email}</p>
             <p>Name: {currentUser.attributes.name}</p>
-            <Link to={{ pathname: `users/${currentUser.attributes.name}/edit`, state: { currentUser } }} >Edit User Info</Link>
         </div>
     )
 }
