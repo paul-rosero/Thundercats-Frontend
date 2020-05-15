649ba196d34163498b665d27f009df80fe6b3a7d
Paul Rosero <paul_rosero@yahoo.com>
2020-05-15 15:59:42 -0400

    add edit link to edit user information.

--- src/components/DisplayUserInfo.js
+++ src/components/DisplayUserInfo.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const DisplayUserInfo = ({currentUser}) => {
     return (
@@ -6,6 +7,7 @@ const DisplayUserInfo = ({currentUser}) => {
             <p>Username: {currentUser.attributes.username}</p>
             <p>Email: {currentUser.attributes.email}</p>
             <p>Name: {currentUser.attributes.name}</p>
+            <Link to={{ pathname: `users/${currentUser.attributes.name}/edit`, state: { currentUser } }} >Edit User Info</Link>
         </div>
     )
 }
