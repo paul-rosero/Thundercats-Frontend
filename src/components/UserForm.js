a314e8fcd3d815662812d54dd94f6180ce1119be
Paul Rosero <paul_rosero@yahoo.com>
2020-05-17 14:39:06 -0400

    add file to use as template form to edit a user and sign up.

---
+++ src/components/UserForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateUserForm } from '../actions/userForm.js'
+
+const UserForm = ({formData, updateUserForm, handleOnSubmit, editMode }) => {
+    const { name, username, email, password } = formData
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        // const updatedFormInfo = {
+        //     ...userForm,
+        //     [name]: value
+        // }
+        // updateSignupForm(updatedFormInfo)
+        updateUserForm(name, value)
+    }
+
+    return (
+        <form onSubmit={ event => { 
+            event.preventDefault() 
+            handleOnSubmit(formData) 
+        }}>
+
+            <label>Name: </label>
+            <input name="name" type="text" value={ name } onChange={ handleInputChange } /><br/>
+
+            <label>Email: </label>
+            <input name="email" type="text" value={ email } onChange={ handleInputChange } /><br/>
+
+            <label>Username: </label>
+            <input name="username" type="text" value={ username } onChange={ handleInputChange } /><br/>
+
+            <label>Password: </label>
+            <input name="password" type="text" value={ password } onChange={ handleInputChange } /><br />
+
+            <input type="submit" value= { editMode ? "Update" : "Sign Up" } />
+
+         </form>
+       
+    )
+}
+
+const mapStateToProps = ({ userForm }) => {
+    return { 
+        formData: userForm
+     }
+}
+
+export default connect(mapStateToProps, { updateUserForm })(UserForm);
