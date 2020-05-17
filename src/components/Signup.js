4889f739ef9b1c5b196f095b9c0207a1c5faad64
Paul Rosero <paul_rosero@yahoo.com>
2020-05-17 14:58:58 -0400

    delete code that was not needed because i changed my form format.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,50 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { updateSignupForm } from '../actions/signupForm.js'
 import { signup } from '../actions/currentUser.js'
 import { signupGreetings } from '../containers/PageGreetings.js';
+import UserForm from './UserForm.js';
 
-const Signup = ({signupForm, updateSignupForm, signup, history }) => {
-    const { name, username, email, password } = signupForm
+const Signup = ({ signup, history }) => {
 
-    const handleInputChange = event => {
-        const { name, value } = event.target
-        const updatedFormInfo = {
-            ...signupForm,
-            [name]: value
-        }
-      updateSignupForm(updatedFormInfo)
-    }
-
-    const handleOnSubmit = event => {
-        event.preventDefault();
-        signup(signupForm, history)
+    const handleOnSubmit = formData => {
+        signup({ ...formData }, history)
     }
    
     return (
         <div className="signup-form">
             { signupGreetings() }
-            <form name="signup-form" onSubmit={handleOnSubmit}>
-                <label>Name: </label>
-                <input name="name" type="text" value={name} onChange={handleInputChange} /><br/>
-
-                <label>Email: </label>
-                <input name="email" type="text" value={email} onChange={handleInputChange} /><br/>
-
-                <label>Username: </label>
-                <input name="username" type="text" value={username} onChange={handleInputChange} /><br/>
-
-                <label>Password: </label>
-                <input name="password" type="text" value={password} onChange={handleInputChange} /><br />
-
-                <input type="submit" value="Sign Up" />
-            </form>
+            <UserForm history={history} handleOnSubmit={handleOnSubmit} />
         </div>
     )
 }
 
-const mapStateToProps = ({ signupForm }) => {
-    return { signupForm }
-}
-
-export default connect(mapStateToProps, { updateSignupForm, signup })(Signup);
+export default connect(null, { signup })(Signup);
