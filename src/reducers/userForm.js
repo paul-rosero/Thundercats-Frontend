67e5e7254f3fc79cc2903b71f29c602c779fa647
Paul Rosero <paul_rosero@yahoo.com>
2020-05-17 14:36:21 -0400

    rename the file from editForm to UserForm.

--- src/reducers/userForm.js
+++ src/reducers/userForm.js
@@ -1,19 +1,20 @@
 const initialState = {
     name: "",
     email: "",
-    username: ""
+    username: "",
+    password: ""
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case "UPDATE_EDIT_FORM" :
+        case "UPDATE_USER_FORM" :
             const res = {
                 ...state,
                 [action.formData.name]: action.formData.value 
             }
             return res
-        // case "RESET_EDIT_FORM" :
-        //     return initialState
+        case "RESET_USER_FORM" :
+            return initialState
         default :
             return state
     }
