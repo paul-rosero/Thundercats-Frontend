9b6fddb54661474f1daf4e7efa2f26fe79a284bc
Paul Rosero <paul_rosero@yahoo.com>
2020-05-18 15:37:19 -0400

    add signup link at the bottom so its user friendly.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { updateLoginForm } from '../actions/loginForm.js'
 import { currentUserLogin } from '../actions/currentUser.js'
 import { loginGreetings } from '../containers/PageGreetings'
+import { Link } from 'react-router-dom'
+
 
 const Login = ({ loginForm, updateLoginForm, currentUserLogin, history }) => {
     const { username, password } = loginForm
@@ -22,7 +24,9 @@ const Login = ({ loginForm, updateLoginForm, currentUserLogin, history }) => {
     }
 
     return (
+        
         <div className="login-form">
+            
             {loginGreetings()}
             <form name="login-form" onSubmit={handleOnSubmit}>
                 <label>Username: </label>
@@ -33,6 +37,7 @@ const Login = ({ loginForm, updateLoginForm, currentUserLogin, history }) => {
             
                 <input type="submit" value="Log In" />
             </form> 
+             <p>or <Link to='/signup'>Signup</Link> if you don't have an account.</p>
         </div>
     )
 }
