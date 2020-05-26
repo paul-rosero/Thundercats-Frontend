1c6ec1367a0c25eb0edf5d4e87c6d51c775b1ed7
Paul Rosero <paul_rosero@yahoo.com>
2020-05-26 16:44:33 -0400

    add characters to CharsHeadShots as props.

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -4,19 +4,21 @@ import CharHeadshots from './CharHeadshots';
 import DisplayUserInfo from '../components/DisplayUserInfo';
 import { profileGreetings } from './PageGreetings';
   
-const ProfilePage = ({currentUser}) => {
+const ProfilePage = ({ currentUser, characters }) => {
     return (
         <div className="profile-page">
             { profileGreetings(currentUser) } 
-             <DisplayUserInfo currentUser={currentUser} />
-            <CharHeadshots currentUser={currentUser}/>
+             <DisplayUserInfo currentUser={ currentUser } />
+            <CharHeadshots currentUser={ currentUser } characters={characters}/>
         </div>
-            
     )
 }
 
 const mapStateToProps = ({currentUser}) => {
-    return {currentUser}
+    return {
+        currentUser,
+        characters: currentUser.relationships.characters
+    }
   }  
 
 export default connect(mapStateToProps)(ProfilePage)
