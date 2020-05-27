2e3cf307fa7573cde8c900d88c840e2938306072
Paul Rosero <paul_rosero@yahoo.com>
2020-05-27 10:57:46 -0400

    delete console.logs.

--- src/components/EditCurrentUser.js
+++ src/components/EditCurrentUser.js
@@ -1,48 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserForm from './UserForm';
 import { connect } from 'react-redux';
 import { editCurrentUser, deleteCurrentUser } from '../actions/currentUser';
 import { setFormDataForEdit, resetUserForm } from '../actions/userForm';
 import { editGreetings } from '../containers/PageGreetings'
-class EditCurrentUser extends Component {
-    componentDidMount(){
-        const { currentUser, setFormDataForEdit } = this.props
-        currentUser && setFormDataForEdit(currentUser)
-      }
-    
-      componentDidUpdate(prevProps) {
-        const { currentUser, setFormDataForEdit } = this.props
-        currentUser && !prevProps.currentUser && setFormDataForEdit(currentUser)
-      }
 
-      componentWillUnmount() {
-        const { resetUserForm } =this.props
-        resetUserForm()
-      }
+const EditCurrentUser = ({ currentUser, deleteCurrentUser, editCurrentUser, history }) => {
 
-    handleOnSubmit = ( event, formData ) => {
-        event.preventDefault()
-        const { editCurrentUser, currentUser, history } = this.props
-        editCurrentUser({
-          ...formData,
-          currentUser
-        }, history)
-      }
+  const handleOnSubmit = ( event, formData ) => {
+    event.preventDefault()
+    editCurrentUser({
+      ...formData,
+      currentUser
+    }, history)
+  }
 
-    render() {
-        const { history, deleteCurrentUser, currentUser } = this.props
-        return (
-            <div className="edit-form">
-                { editGreetings() }
-                <UserForm editMode handleOnSubmit={ this.handleOnSubmit } />
-                <button style={{ color: "red" }} onClick={()=>deleteCurrentUser(currentUser, history)}>Delete Account</button>
-            </div>
-        )
-    }
+  return (
+    <div className="edit-form">
+      { editGreetings() }
+      <UserForm editMode handleOnSubmit={ handleOnSubmit } />
+      <button style={{ color: "red" }} onClick={()=>deleteCurrentUser(currentUser, history)}>Delete Account</button>
+    </div>
+  )
 }
 
-const mapStateToProps = ({ currentUser }) => {
-  return { currentUser }
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  }
 }
 
 export default connect(mapStateToProps, { resetUserForm, setFormDataForEdit, editCurrentUser, deleteCurrentUser })(EditCurrentUser)
