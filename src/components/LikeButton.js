ed7613206cafd5589c994d79b24367e376ee6e2a
Paul Rosero <paul_rosero@yahoo.com>
2020-06-20 15:15:56 -0400

    add button to be able to like and unlike

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -6,11 +6,16 @@ const LikeButton = (props) => {
     }
 
         return (
-            <div className="like-button" >
-                {console.log('props.character2', props.character)}
-                
-                <button onClick={ handleOnClick } >{ props.character.attributes.favorite ? "UnLike Me" : "Like Me" }</button>
-            </div>
+            <>
+                <div className="favorite-heart">
+                    { character.attributes.favorite ? <img className='favorite-heart' src={process.env.PUBLIC_URL + '/favorite.png'} alt='favorite-heart' />  : <img className='unfavorite-heart' src={process.env.PUBLIC_URL + '/unfavorite.png'} alt='unfavorite-heart' /> }
+                </div>
+                <div className="like-button" >
+                    {console.log('props.character2', props.character)}
+                    
+                    <button onClick={ handleOnClick } >{ props.character.attributes.favorite ? "UnLike Me" : "Like Me" }</button>
+                </div>
+            </>
         )
     
 }
