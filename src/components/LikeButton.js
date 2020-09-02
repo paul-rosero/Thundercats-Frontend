e5ad9b158d03f193f3af799142a5135336cacd4e
Paul Rosero <paul_rosero@yahoo.com>
2020-09-02 18:50:02 -0400

    add props.to line 11 first word

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -8,7 +8,7 @@ const LikeButton = (props) => {
         return (
             <>
                 <div className="favorite-heart">
-                    { character.attributes.favorite ? <img className='favorite-heart' src={process.env.PUBLIC_URL + '/favorite.png'} alt='favorite-heart' />  : <img className='unfavorite-heart' src={process.env.PUBLIC_URL + '/unfavorite.png'} alt='unfavorite-heart' /> }
+                    { props.character.attributes.favorite ? <img className='favorite-heart' src={process.env.PUBLIC_URL + '/favorite.png'} alt='favorite-heart' />  : <img className='unfavorite-heart' src={process.env.PUBLIC_URL + '/unfavorite.png'} alt='unfavorite-heart' /> }
                 </div>
                 <div className="like-button" >
                     {console.log('props.character2', props.character)}
