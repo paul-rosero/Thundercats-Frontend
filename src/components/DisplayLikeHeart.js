fe3ca093a5c44512bea1517d1bda3dbff64bbf5c
Paul Rosero <paul_rosero@yahoo.com>
2020-09-09 18:32:49 -0400

    erase  favorite from line 5 and leave it as heart

--- src/components/DisplayLikeHeart.js
+++ src/components/DisplayLikeHeart.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export const DisplayLikeHeart = ({ character }) => {
+    console.log(character.attributes.favorite )
     return (
-        <div className="favorite-heart">
-           { character.attributes.favorite ? <img className='favorite-heart' src={process.env.PUBLIC_URL + '/favorite.png'} alt='favorite-heart' />  : <img className='unfavorite-heart' src={process.env.PUBLIC_URL + '/unfavorite.png'} alt='unfavorite-heart' /> }
+        <div className="heart">
+           { character.attributes.favorite ? <img className='favorite-heart' src={process.env.PUBLIC_URL + '/favorite.png'} alt='favorite-heart' /> : <img className='unfavorite-heart' src={process.env.PUBLIC_URL + '/unfavorite.png'} alt='unfavorite-heart' /> }
         </div>
     )
 }
