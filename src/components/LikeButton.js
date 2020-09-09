c922b62e4292e51f152ac1d354a6ce2496687e61
Paul Rosero <paul_rosero@yahoo.com>
2020-09-09 18:31:52 -0400

    delete div classname favorite-heart

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -2,16 +2,13 @@ import React from 'react'
 
 const LikeButton = (props) => {
     const handleOnClick = () => {
-       props.character.attributes.favorite = !props.character.attributes.favorite
+       return props.character.attributes.favorite = !props.character.attributes.favorite
     }
 
         return (
             <>
-                <div className="favorite-heart">
-                    { props.character.attributes.favorite ? <img className='favorite-heart' src={process.env.PUBLIC_URL + '/favorite.png'} alt='favorite-heart' />  : <img className='unfavorite-heart' src={process.env.PUBLIC_URL + '/unfavorite.png'} alt='unfavorite-heart' /> }
-                </div>
                 <div className="like-button" >
-                    {console.log('props.character2', props.character)}
+                    {console.log('props.character2', props.character.attributes.favorite)}
                     
                     <button onClick={ handleOnClick } >{ props.character.attributes.favorite ? "UnLike Me" : "Like Me" }</button>
                 </div>
