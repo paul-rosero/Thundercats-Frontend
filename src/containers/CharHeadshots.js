2ff827e77d8c5e58a925236654f1b5cef9379e70
Paul Rosero <paul_rosero@yahoo.com>
2020-09-10 13:15:11 -0400

    add currentUser & setFavorite to <LikeButton>

--- src/containers/CharHeadshots.js
+++ src/containers/CharHeadshots.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Link} from 'react-router-dom'
 import LikeButton from '../components/LikeButton'
 import { DisplayLikeHeart } from '../components/DisplayLikeHeart'
+import { setFavorite } from '../actions/characters'
 
-const CharHeadshots = ({ characters }) => {
+const CharHeadshots = ({ characters, currentUser }) => {
     const mappedChars = characters.map((character, index) => 
         <li key={ index } className="character">
             <Link 
@@ -13,7 +14,7 @@ const CharHeadshots = ({ characters }) => {
                 }}><img key={`${character.attributes.name}`} src={process.env.PUBLIC_URL + `/character_images/${character.attributes.name}.jpg`} alt={`${character.attributes.name}`} />
             </Link>  
             <DisplayLikeHeart character={character} />
-            <LikeButton character={character}/>
+            <LikeButton character={character} currentUser={currentUser} setFavorite={setFavorite}/>
         </li>
     )
  
